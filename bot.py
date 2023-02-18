826e9ff808ae1c10e08d6bb82f4e367dac52dff3
Miandic <misha.andreev.novo@gmail.com>
2023-02-18 13:31:07 +0300

    Text + images + exceptions

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import urlextract
 from aiogram import Bot, Dispatcher, executor, types
 from random import randint
 from qrdetector import detect
@@ -35,17 +36,32 @@ async def photo(message: types.Message):
     await message.photo[-1].download(name)
     msg = await message.reply("I'm checking your QR code")
     val = detect(name)
-    for i in val[1]:
-        print(i)
     if val[0]:
         ans = ""
         for i in val[1]:
-            #check linnk for security
-            ans += i + "\nChecked:\n\nSSL certificate: " '''check SSL certificate''' + "\nProtocol: " '''check Protocol''' + "\nRedirects: " '''check Redirects''' + "\n\n" + "Result: " '''check Result''' + "\n\n"
+            extractor = urlextract.URLExtract()
+            url = extractor.find_urls(i)
+            if url:
+                #check link for security
+                ans += i + "\nChecked:\n\nSSL certificate: " '''check SSL certificate''' + "\nProtocol: " '''check Protocol''' + "\nRedirects: " '''check Redirects''' + "\n\n" + "Result: " '''check Result''' + "\n\n"
+            else:
+                ans += i +" is not a URL ¯\_(ツ)_/¯" + "\n\n"
         
         await msg.edit_text(ans)
     else:
-        await msg.edit_text("QR code is not detected")
+        await msg.edit_text("QR code is not detected ＞﹏＜")
+
+
+@dp.message_handler(content_types=['text'])
+async def text(message: types.Message):
+    extractor = urlextract.URLExtract()
+    urls = extractor.find_urls(message.text)
+    if urls:
+        for i in urls:
+            #check link for security
+            await message.reply(i + "\nChecked:\n\nSSL certificate: " '''check SSL certificate''' + "\nProtocol: " '''check Protocol''' + "\nRedirects: " '''check Redirects''' + "\n\n" + "Result: " '''check Result''' + "\n\n")
+    else:
+        await message.reply("No URLs in your message :)")
 
 
 if __name__ == '__main__':
