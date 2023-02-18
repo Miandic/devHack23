050a782fe4a28c3a8711be586205f24910e7ac46
miandic <miandicbiz@gmail.com>
2023-02-19 00:37:39 +0300

    Even better SSL, redirect test WIP

--- checker.py
+++ checker.py
@@ -1,28 +1,43 @@
-import requests
+import ssl, socket, requests
 
-def SSL(url):
+def CheckSSL(link):
+    ctx = ssl.create_default_context()
+    s = ctx.wrap_socket(socket.socket(), server_hostname=link)
     try:
-        requests.get(url)
-    except requests.exceptions.SSLError:
-        return False
-    except Exception:
-        return True
-    return True
+        s.connect((link, 443))
+        cert = s.getpeercert()
+
+        subject = dict(x[0] for x in cert['subject'])
+        try:
+            subject["businessCategory"]
+            return 2
+        except KeyError:
+            return 1
+    except ssl.SSLCertVerificationError:
+        return 0
 
 
-def normalize(url):
-    if url[0:4] == 'http':
-        return url
-    else:
-        return "https://"+url
+def CheckRedirects(link):
+    try:
+        r = requests.get(link)
+    except requests.exceptions.MissingSchema:
+
+
+
+
 
 
 def checkURL(link):
-    link = normalize(link)
     ret = {
-        "SSL": SSL(link),
-        "Protocol": 'hz'
+        "SSL": 1,
+        "Protocol": True,
+        "Reachable": True
     }
+
+    ret["SSL"] = CheckSSL(link)
+    CheckRedirects(link)
     return ret
 
 print(checkURL('info.cern.ch'))
+print(checkURL('google.com'))
+print(checkURL('thawte.com'))
